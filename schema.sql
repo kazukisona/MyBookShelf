9407cbfee00547cae5dcca22e6cecbf368301e95
Kazuki Sona <kazukisona@gmail.com>
2017-01-22 16:46:54 -0600

    Schema for a table

---
+++ schema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE tb_book(
+	id INT NOT NULL AUTO_INCREMENT,
+	author VARCHAR(200) NOT NULL,
+	publisher VARCHAR(100) NOT NULL,
+	published_date DATE,
+	isbn VARCHAR(20) NOT NULL,
+	buying_date DATE,
+	PRIMARY KEY(id)
+);
